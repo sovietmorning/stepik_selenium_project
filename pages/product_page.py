e4a26a011b450c4ff13d6f01b79d0d59f842a9c6
nannadym <nannadymova@yandex.ru>
2025-01-11 12:59:00 +0300

    Add parametrized tests for product page

--- pages/product_page.py
+++ pages/product_page.py
@@ -13,21 +13,21 @@ class ProductPage(BasePage):
         title_in_message = self.browser.find_element(
             *ProductPageLocators.PRODUCT_TITLE_IN_SUCCESS_MESSAGE
         )
-        assert (
-            title.text == title_in_message.text
-        ), "The title in success message differs from the product title"
         print(
             f"The product title is: '{title.text}', the title in message is: '{title_in_message.text}'"
         )
+        assert (
+            title.text == title_in_message.text
+        ), "The title in success message differs from the product title"
 
     def should_be_right_price_in_success_message(self):
         price = self.browser.find_element(*ProductPageLocators.PRICE)
         price_in_message = self.browser.find_element(
             *ProductPageLocators.PRICE_BASKET_TOTAL
         )
-        assert (
-            price.text == price_in_message.text
-        ), "The price in success message differs from the product price"
         print(
             f"The product price is: '{price.text}', the price in message is: '{price_in_message.text}'"
         )
+        assert (
+            price.text == price_in_message.text
+        ), "The price in success message differs from the product price"
